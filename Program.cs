e9cb4dc9f140c2930212eef73badbe13423b18ab
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:04:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.TheReturnKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cube(5));
+        }
+
+        static int Cube(int n) { 
+         int result = n*n*n;
+            return result;
+        }
+    }
+}
